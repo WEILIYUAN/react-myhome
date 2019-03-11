d09dd73facf1aaa26291b6b09312123f2a46783d
weiliyuan <1277310748@qq.com>
2019-03-11 19:50:18 +0800

    完成首页路由配置

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter, Route,Switch } from "react-router-dom";
 import './App.css';
+
+import Login from './login'
+import Main from './module/main'
 
 class App extends Component {
   render() {
     return (
-      <div >
-        初始化
-      </div>
+      <BrowserRouter>
+      <Switch>
+    
+      <Route path="/" exact component={Login}></Route>
+      <Route path="/home"  component={Main}></Route>
+      </Switch>
+    
+      </BrowserRouter>
     );
   }
 }
